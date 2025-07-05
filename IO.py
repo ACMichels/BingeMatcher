4ceff9443be0ac9b387bed7c59af79708774b5ee
Andor <andor.michels@redian.nl>
2025-07-05 20:37:08 +0200

    Create cache folder structure if it does not exist

--- IO.py
+++ IO.py
@@ -1,17 +1,27 @@
 import os
 import pickle
 
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def update_cache_structure():
+    create_dir("Cache/")
+    create_dir("Cache/Images/")
 
 def cache_data(data, filename):
+    update_cache_structure()
     path = f"Cache/{filename}.pkl"
     with open(path, 'wb') as file:
         pickle.dump(data, file)
 
 def get_cache_exists(filename):
+    update_cache_structure()
     path = f"Cache/{filename}.pkl"
     return os.path.isfile(path)
 
 def get_cached_data(filename):
+    update_cache_structure()
     path = f"Cache/{filename}.pkl"
     with open(path, 'rb') as file:
         return pickle.load(file)
